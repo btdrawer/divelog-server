747f9e29983a80cd034de6f0ac4fc7fac865e447
Ben Drawer <bendrawer1@gmail.com>
2021-04-24 13:19:16 +0100

    Remove broken references

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,8 +1,1 @@
-import runListQuery, { ListResult } from "./runListQuery";
-
-export { runListQuery, ListResult };
-
-export * from "./authUtils";
-export * from "./base64Utils";
 export * from "./constants";
-export * from "./routeUtils";
